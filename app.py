30c3d2ecc59c224eecbe99b5c8546625fff8419d
Emily <emily.butera@ASCTE.org>
2025-06-23 14:24:31 -0500

    Added new page
    Added whole new page to use to find triadic color combinations!

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/triadic')
+def triadic():
+    return render_template("tri.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
