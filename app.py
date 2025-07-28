e0892ba8716b1c1c4ad0598473abccdf0f100ded
Emily <emily.butera@ASCTE.org>
2025-07-28 11:57:17 -0500

    FIXED

--- app.py
+++ app.py
@@ -33,3 +33,9 @@ def aboutSite():
 @app.route('/random')
 def random():
     return render_template("rand.html")
+
+# Only run Flask's built-in server in local development
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 8000))
+    app.run(debug=True, host='0.0.0.0', port=port)
